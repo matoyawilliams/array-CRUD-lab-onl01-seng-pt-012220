e231c0af4962f9960ec3080b4e2b076549a1ad26
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-28 01:50:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def remove_element_from_end_of_array(array)
   array.pop(element)
 end
 
-def remove_element_from_start_of_array
+def remove_element_from_start_of_array(array)
   array.shift(element)
 end
 
