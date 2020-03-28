28a37b5229834421f9648a0f4f1925139b57f5fb
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-28 01:31:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,7 +2,7 @@ def create_an_empty_array
   []
 end
 
-def create_an_array = ["apple","banana","orange"] 
-create_an_array << "strawberry"
-end
-end
+def create_an_array
+  ["apple","banana","orange"] 
+end 
+
