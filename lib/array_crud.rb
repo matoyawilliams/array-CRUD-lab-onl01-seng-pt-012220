b1d3c152daad9147ad98b612398f8a55d3aaaeb6
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-28 01:16:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,10 +3,4 @@ end
 
 def create_an_array = ["apple", "banana", "orange"]
 end
-
-def add_element_to_end_of_array = ["apple", "banana", "orange"]
-  add_element_to_end_of_array << "berry"
-  
-  puts add_element_to_end_of_array.inspect 
-end 
 end
