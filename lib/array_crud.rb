d62cabb2dbe9a429be4898c3c32c6dcaba94013e
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-28 01:34:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,3 +6,9 @@ def create_an_array
   ["apple", "banana", "orange", "strawberry"] 
 end 
 
+def add_element_to_end_of_array
+  ["apple", "banana", "orange", "strawberry"]
+  
+  add_element_to_end_of_array << "kiwi"
+end
+
