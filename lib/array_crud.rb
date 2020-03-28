91a331355ee76790a532644309286cb730f4279c
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-28 00:43:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,10 +1,1 @@
-def create_an_empty_array[] 
-end
-
-def add_element_to_end_of_array["1","2","3","4"]
-  
-  add_element_to_end_of_array << "5"
-  puts add_element_to_end_of_array.inspect
-end
-end
 
